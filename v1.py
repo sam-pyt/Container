c92bda66b182cc19f187f05c59c4be6f61bf24c8
sam-pyt <32972589+sam-pyt@users.noreply.github.com>
2017-10-20 22:35:01 -0400

    Update v1.py

--- v1.py
+++ v1.py
@@ -13,6 +13,7 @@ class Container():
         self.items=dict(zip(list0,list1))
 
     def add(self,key,value):
+        'add an item to the container'
         self.items[key]=value
 
     def remove(self,item):
@@ -23,28 +24,37 @@ class Container():
         return None
 
     def __add__(self,other):
+        'allow another container or dict object to be added to self.'
         dictionary=self.items
-        for k,v in other.keys(),other.values():
-            dictionary[k]=v
+        other=other if type(other) == dict else other.items
+        for k in other.keys():
+            dictionary[k]=other[k]
         return dictionary
 
     def __radd__(self,other):
+        'allow self to be added to another container'
         dictionary=self.items
-        for k,v in other.keys(),other.values():
-            dictionary[k]=v
+        other=other if type(other) == dict else other.items
+        for k in other.keys():
+            dictionary[k]=other[k]
         return dictionary
 
     def __iadd__(self,other):
+        'allows the += operator to be used.'
         self.items=self+other
 
     def __getitem__(self,key):
+        'gets the item at specified index in self.items... allows for the key to be either the value or actual key'
         return self.items[key] if key in self.items.keys() else self.getKey(self.items,key)
 
     def __str__(self):
+        'when print is used on self, the contents of self.items will print.'
         return str(self.items)
 
     def __repr__(self):
+        'string form of self is the contents of self.items'
         return str(self.items)
 
     def __len__(self):
+        'get the length of self.items'
         return len(self.items)
